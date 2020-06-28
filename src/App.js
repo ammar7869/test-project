b490d8e8ab51a4ca83a68de5b1451f60cc9d5483
ammar7869 <ak5797229@gmail.com>
2020-06-28 14:53:33 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>my appp</p>
+    </div>
+  );
+}
+
+export default App;
